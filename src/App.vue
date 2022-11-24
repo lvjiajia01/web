07f31b2998788f279c9f43176707ef3257107ffd
DESKTOP-65L9TO1\lvjiajia <lvjiajia@pwrd.com>
2022-11-24 20:09:54 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -22,7 +22,7 @@ export default {
     }
   },
   async mounted() {
-    const res = await axios.get("/api/users")
+    const res = await axios.get("http://localhost:3000/api/users")
     this.users = res.data
   }
 }
